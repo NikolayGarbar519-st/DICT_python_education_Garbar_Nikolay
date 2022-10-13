40ee63f2eed79308a636036bb1418bd3226323f9
NikolayGarbar519-st <m.a.harbar@student.khai.edu>
2022-10-13 18:22:43 +0300

    ChatBot 5 stage+check number

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -18,4 +18,24 @@ while i<num:
     i+=1
 else:
     print(str(i)+"!")
-    print("complited!")
+    print("Completed!")
+# test,text from practice
+print("Let's test your programming knowledge.")
+print("Why do we use methods?")
+print("1. To repeat a statement multiple times.")
+print("2. To decompose a program into several small subroutines.")
+print("3. To determine the execution time of a program.")
+print("4. To interrupt the execution of a program.")
+ans=int(input())
+while ans!=2:
+    print("Please,try again")
+    if ans>=5:
+        print("Please,write number from 1 to 4")
+        ans = int(input())
+    elif ans==0:
+        print("Please,write number from 1 to 4")
+        ans = int(input())
+    else:
+        ans = int(input())
+else:
+    print("Congratulations, have a nice day!")
